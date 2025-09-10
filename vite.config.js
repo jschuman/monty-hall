5da0dd877f286936297d9bc00dfa4a29bf364522
Jeff Schuman <jschuman@solutionstreet.com>
2025-09-10 16:07:10 -0400

    updated web page title

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/monty-hall/', // Set base path for subdirectory deployment
   server: {
     allowedHosts: [
       'localhost',
       '127.0.0.1',
-      '5feb7425d717.ngrok-free.app', // add your ngrok hostname here
+      //'5feb7425d717.ngrok-free.app', // add your ngrok hostname here
     ],
   },
 })
